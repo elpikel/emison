9b9a76c4b592d9262b1f1f5357c3bf1ea2c1a804
Arkadiusz Plichta <el.pikel@gmail.com>
2019-05-19 16:18:49 +0200

    sample chat client screens

--- emison_mobile/App.js
+++ emison_mobile/App.js
@@ -1,21 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// Import the screens
+import Main from './components/Main';
+import Chat from './components/Chat';
+// Import React Navigation
+import { createStackNavigator, createAppContainer } from 'react-navigation'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+// Create the navigator
+const AppNavigator = createStackNavigator({
+  Main: { screen: Main },
+  Chat: { screen: Chat },
 });
+
+// Export it as the root component
+export default createAppContainer(AppNavigator);
---
+++ emison_mobile/components/Main.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
+class Main extends Component {
+    state = { name: '' } 
+    onChangeText = name => this.setState({ name });
+    onPress = () => {
+      this.props.navigation.navigate('Chat', { name: this.state.name });
+    }
+    render() {
+      return (
+        <View>
+          <Text style={styles.title}>Enter your name:</Text>
+          <TextInput
+            onChangeText={this.onChangeText}
+            style={styles.nameInput}
+            placeHolder="John Cena"
+            value={this.state.name}
+          />
+          <TouchableOpacity onPress={this.onPress}>
+            <Text style={styles.buttonText}>Next</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+}
+export default Main;
+const offset = 24;
+const styles = StyleSheet.create({
+  nameInput: { // 3. <- Add a style for the input
+    height: offset * 2,
+    margin: offset,
+    paddingHorizontal: offset,
+    borderColor: '#111111',
+    borderWidth: 1,
+  },
+  title: { // 4.
+    marginTop: offset,
+    marginLeft: offset,
+    fontSize: offset,
+  },
+  buttonText: { // 5.
+    marginLeft: offset,
+    fontSize: offset,
+  },
+});
